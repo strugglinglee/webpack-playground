a9ea7e609fe12f961128aa1b9917000880fa2257
strugglinglee <strugglinglee@163.com>
2024-04-08 14:00:28 +0800

    feat: 使用html-webpack-plugin插件，自动创建index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
-const path = require('path');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: {
-    index: './src/index.js',
-    print: './src/print.js'
+    index: "./src/index.js",
+    print: "./src/print.js",
   },
   output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  }
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [new HtmlWebpackPlugin({ title: "管理输出" })],
 };
