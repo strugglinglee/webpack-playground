1b3694c803c9bd876cb870e946020d5b01cccd97
strugglinglee <strugglinglee@163.com>
2024-04-09 14:39:30 +0800

    feat: 清理文件

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import _ from "lodash";
-import Print from "./print";
 
 function component() {
   const element = document.createElement("div");
-
   // lodash 现在使用 import 引入
   element.innerHTML = _.join(["Hello", "webpack"], " ");
-  element.onclick = Print.bind(null, "Hello webpack!");
-  console.log('hihi')
   return element;
 }
 
